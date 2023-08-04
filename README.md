18c61736541f67bb4c517dd35ff474c0dc14a2ac
TomFirth <tom.firth84@gmail.com>
2023-08-04 18:56:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# NightSky
+
+Simple script created to capture night photos with a raspbery pi.
+
+`npm start`: runs main script and creates server for you to view photos: (http://localhost:3000)[local server]
+`npm run photo`: takes single photo.
+`npm run video`: takes 1 minute video.
---
+++ index.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const CronJob = require('cron').CronJob;
+
+const photo = require('./photo')
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const directory = './photos';
+
+const server = http.createServer((req, res) => {
+  photoCron()
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  getDir(res)
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+function getDir(res) {
+  fs.readdir(directory, (error, files) => {
+    if (error) console.error(error)
+    let body = ""
+    files.forEach(file => {
+      let fileDetails = fs.lstatSync(path.resolve(directory, file));
+      if (fileDetails.isDirectory()) {
+        body += 'Directory: ' + file + '\r\n';
+      } else {
+        body += file + '\r\n';
+      }
+    });
+    res.end(body)
+  });
+}
+
+function photoCron() {
+  const job = new CronJob(
+    '0 */15 18-23,0-6 * * *',
+    function() {
+      photo.snap()
+    }
+  );
+  job.start()
+}
